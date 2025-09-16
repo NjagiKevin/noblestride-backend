6ce229aaf7d4e7b88b869f4e0dbd91b796113ca6
Victor Karanja <victor.karanja@webmasters.co.ke>
2025-09-16 17:37:09 +0300

    Everything notifications.

--- Routes/notificationRoutes.js
+++ Routes/notificationRoutes.js
@@ -1,12 +1,13 @@
 // Routes/notificationRoutes.js
 const express = require("express");
 const notificationController = require("../Controllers/notificationController");
-const { getUserNotifications, markNotificationAsRead } = notificationController;
+const { getUserNotifications, markNotificationAsRead, markAllNotificationsAsRead } = notificationController;
 const authMiddleware = require("../Middlewares/authMiddleware");
 
 const router = express.Router();
 
 router.get("/", authMiddleware, getUserNotifications);
 router.put("/:id/read", authMiddleware, markNotificationAsRead);
+router.put("/read-all", authMiddleware, markAllNotificationsAsRead);
 
 module.exports = router;
