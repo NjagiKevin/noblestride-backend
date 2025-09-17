4c14eebb799e03dfc531cb7dcbbccb89ca248b37
Victor Karanja <victor.karanja@webmasters.co.ke>
2025-09-17 16:57:33 +0300

    Milestone table migration fix.

--- migrations/20250917131925-add-created-by-to-milestones.js
+++ migrations/20250917131925-add-created-by-to-milestones.js
@@ -2,11 +2,28 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn('milestones', 'created_by', {
+      type: Sequelize.INTEGER,
+      allowNull: true, // Temporarily allow null
+      references: {
+        model: 'users',
+        key: 'id',
+      },
+      onUpdate: 'CASCADE',
+      onDelete: 'SET NULL',
+    });
+
     // Update existing rows to a default value (e.g., the ID of an admin user)
     await queryInterface.bulkUpdate('milestones', { created_by: 1 }, { created_by: null });
+
+    // Alter the column to not allow null values
+    await queryInterface.changeColumn('milestones', 'created_by', {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+    });
   },
 
   down: async (queryInterface, Sequelize) => {
-    // There is no need to revert this change
+    await queryInterface.removeColumn('milestones', 'created_by');
   },
 };
